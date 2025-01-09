72e4a7a67330ccafc5c5ff064537953bb6775256
saintramon <ramonjasmin8@gmail.com>
2025-01-09 10:56:27 +0800

    Cleaner works but not for multi line reviews

--- cleaner.py
+++ cleaner.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-input_file = 'letterboxd-anora'
-output_file = 'letterboxd-anora-cleaned'
+input_file = 'letterboxd-anora.csv'
+output_file = 'letterboxd-anora-cleaned.csv'
 
 
 reviews_df = pd.read_csv(input_file)
 
 
-reviews_df[reviews]
+reviews_df['reviews'] = reviews_df['reviews'].str.replace('"', '', regex=False)
+
+reviews_df.to_csv(output_file, index=False, encoding='utf-8')
